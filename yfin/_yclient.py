84048c858dbca881c4ae8784a042816fb447104e
Jocelyn Legault <jocelynlegault@gmail.com>
2023-09-22 18:23:44 -0400

    Better use of the requests API in YClient

--- yfin/_yclient.py
+++ yfin/_yclient.py
@@ -24,52 +24,43 @@ class YClient:
 
     def __init__(self) -> None:
         self._http_client: requests.Session = requests.Session()
+        self._http_client.headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection": "keep-alive",
+            "Content-Type": "text/plain",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "TE": "trailers",
+            "User-Agent": self._USER_AGENT,
+        }
 
         self._expiry: datetime.datetime = datetime.datetime(1970, 1, 1)
-        self._cookies: str = ""
         self._crumb: str = ""
 
-    def __fetch_cookies(self) -> tuple[str, datetime.datetime]:
+    def __refresh_cookies(self) -> None:
         """
-        Fetch cookies from Yahoo Finance.
+        Refreshes cookies from Yahoo Finance.
 
         The cookies are required to fetch the crumb that is in turn required to fetch quotes.
-
-        Returns:
-            tuple[str, datetime.datetime]: The cookies and their expiry.
         """
 
-        logging.debug("Fetching cookies...")
+        logging.debug("Refreshing cookies")
 
-        request = requests.Request(
-            "GET",
+        with self._http_client.get(
             self._COOKIE_URL,
-            headers={
-                "Accept": "*/*",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Connection": "keep-alive",
-                "Content-Type": "text/plain",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-                "TE": "trailers",
-                "User-Agent": self._USER_AGENT,
-            },
-        ).prepare()
-
-        with self._http_client.send(
-            request, timeout=self._DEFAULT_HTTP_TIMEOUT
+            timeout=self._DEFAULT_HTTP_TIMEOUT,
         ) as response:
             # default expiry is ten years in the future
             expiry = datetime.datetime.now() + datetime.timedelta(days=3650)
-            result = ""
 
             try:
                 response.raise_for_status()
             except requests.exceptions.HTTPError as e:
-                logging.error("Can't fetch cookies: %s", e)
-                return result, expiry
+                logging.error("Can't refresh cookies: %s", e)
+                return
 
             for cookie in response.cookies:
                 logging.debug("Considering cookie: %s", cookie)
@@ -83,12 +74,6 @@ class YClient:
 
                 # compute expiry as now + cookie expiry
                 cookie_expiry = datetime.datetime.fromtimestamp(cookie.expires)
-                result += (
-                    cookie.name
-                    + "="
-                    + (cookie.value if cookie.value is not None else "")
-                    + "; "
-                )
 
                 # if the cookie expires afthe the current expiry, don't update the expiry
                 if cookie_expiry >= expiry:
@@ -102,8 +87,7 @@ class YClient:
                 )
                 expiry = cookie_expiry
 
-            result = result.rstrip("; ")
-            return result, expiry
+            self._expiry = expiry
 
     def __fetch_crumb(self) -> str:
         """
@@ -113,28 +97,10 @@ class YClient:
             str: The retrieved crumb.
         """
 
-        logging.debug("Fetching crumb with cookies: %s", self._cookies)
+        logging.debug("Fetching crumb")
 
-        request = requests.Request(
-            "GET",
-            self._CRUMB_URL,
-            headers={
-                "Accept": "*/*",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Connection": "keep-alive",
-                "Content-Type": "text/plain",
-                "Cookie": self._cookies,
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-                "TE": "trailers",
-                "User-Agent": self._USER_AGENT,
-            },
-        ).prepare()
-
-        with self._http_client.send(
-            request, timeout=self._DEFAULT_HTTP_TIMEOUT
+        with self._http_client.get(
+            self._CRUMB_URL, timeout=self._DEFAULT_HTTP_TIMEOUT
         ) as response:
             try:
                 response.raise_for_status()
@@ -149,7 +115,7 @@ class YClient:
         Refresh the crumb required to fetch quotes."""
 
         logging.debug("Refreshing crumb...")
-        self._cookies, self._expiry = self.__fetch_cookies()
+        self.__refresh_cookies()
         self._crumb = self.__fetch_crumb()
 
         if self._crumb is not None and self._crumb != "":
@@ -161,55 +127,20 @@ class YClient:
         else:
             logging.debug("Crumb refresh failed")
 
-    def _new_request(self, api_call: str) -> requests.PreparedRequest:
+    def _execute_request(self, api_call: str) -> dict[str, Any]:
         """
-        Create a new request object for the given api_call.
+        Execute the given request and return the data from the response.
 
         Args:
             api_call (str): The path to the Yahoo! finance API call (with arguments) to create a request for.
 
         Returns:
-            requests.PreparedRequest: The prepared request object.
-        """
-
-        logging.debug("Creating new request for %s", api_call)
-
-        url = self._YAHOO_FINANCE_URL + api_call
-        req = requests.Request(
-            "GET",
-            url,
-            headers={
-                "Accept": "*/*",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Connection": "keep-alive",
-                "Content-Type": "text/plain",
-                "Cookie": self._cookies,
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-                "TE": "trailers",
-                "User-Agent": self._USER_AGENT,
-            },
-        )
-        return req.prepare()
-
-    def _execute_request(self, req: requests.PreparedRequest) -> dict[str, Any]:
-        """
-        Execute the given request and return the data from the response.
-
-        Args:
-            req (requests.PreparedRequest): The request to execute.
-
-        Returns:
             dict[str, Any]: The JSON response.
         """
-        logging.debug("Executing request %s %s", req.method, req.url)
-
         start = datetime.datetime.now()
 
-        with self._http_client.send(
-            req, timeout=self._DEFAULT_HTTP_TIMEOUT
+        with self._http_client.get(
+            self._YAHOO_FINANCE_URL + api_call, timeout=self._DEFAULT_HTTP_TIMEOUT
         ) as response:
             logging.debug("Completed in %s", datetime.datetime.now() - start)
 
@@ -257,6 +188,4 @@ class YClient:
             query_string = urllib.parse.urlencode(query_params)
             api_url += "?" + query_string
 
-        req = self._new_request(api_url)
-
-        return self._execute_request(req)
+        return self._execute_request(api_url)
