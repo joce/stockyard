796571bb1cbc88bbcf0d4340fd356e1f11cb742d
Jocelyn Legault <jocelynlegault@gmail.com>
2023-09-29 23:32:00 -0400

    Playing around with threads (commented out atm)

--- appui/stockyardapp.py
+++ appui/stockyardapp.py
@@ -8,6 +8,10 @@ from textual.widgets import Footer
 from ._quotetable import QuoteTable
 from .stockyardapp_state import StockyardAppState
 
+# import threading
+# import time
+
+
 logging.basicConfig(
     level="NOTSET",
     handlers=[TextualHandler()],
@@ -24,6 +28,12 @@ class StockyardApp(App):
         super().__init__()
         self._state: StockyardAppState = state
 
+        # self._counter: int = 0
+        # self._run_counter: bool = True
+        # self._count_thread: threading.Thread = threading.Thread(
+        #     target=self.increment_counter
+        # )
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield QuoteTable(self._state.quote_table_state())
@@ -32,7 +42,17 @@ class StockyardApp(App):
     def on_mount(self) -> None:
         """Handle mount events."""
         self.title = self._state.title
+        # self._count_thread.start()
+
+    # def increment_counter(self) -> None:
+    #     while self._run_counter:
+    #         self._counter += 1
+    #         lbl: Label = self.query_one("#counter", Label)
+    #         self.call_from_thread(lbl.update, str(self._counter))
+    #         time.sleep(1)
 
     def action_exit(self) -> None:
         """Handle exit actions."""
+        # self._run_counter = False
+        # self._count_thread.join()
         self.exit()
