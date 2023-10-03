72097ea2a09bd425865d3d9b0bf776eb4fdfb41c
Jocelyn Legault <jocelynlegault@gmail.com>
2023-10-02 22:31:33 -0400

    Threaded quote queries.

--- appui/_quotetable.py
+++ appui/_quotetable.py
@@ -6,18 +6,43 @@ from .quotetable_state import QuoteTableState
 
 
 class QuoteTable(DataTable):
+    """
+    A DataTable for displaying quotes
+    """
+
     def __init__(self, state: QuoteTableState) -> None:
         super().__init__()
         self._state: QuoteTableState = state
+        self._version: int
 
     def on_mount(self) -> None:
+        """
+        The event handler called when the widget is added to the app
+        """
+        super().on_mount()
         column: Column
         for column in self._state.columns:
             # TODO Unsure if we put this here on in the state.
             styled_column: Text = Text(column.name, justify=column.justification.value)
             self.add_column(styled_column, width=column.width, key=column.key)
 
+        self.cursor_type = "row"
+        self.zebra_stripes = True
+        self.set_interval(1, self._update_table)
+
+        self._version = self._state.version - 1
+        self._state.query_thread_running = True
+
+    def _on_unmount(self) -> None:
+        self._state.query_thread_running = False
+        super()._on_unmount()
+
+    def _update_table(self) -> None:
+        if self._version == self._state.version:
+            return
+
         quote: list[str]
+        self.clear()  # suboptimal. We need to actually update the table
         for quote in self._state.get_quotes():
             # TODO This is certainly suboptimal to have to zip the quote and the columns together every time.
             stylized_row: list[Text] = [
@@ -25,5 +50,3 @@ class QuoteTable(DataTable):
                 for cell, column in zip(quote, self._state.columns)
             ]
             self.add_row(*stylized_row)
-        self.cursor_type = "row"
-        self.zebra_stripes = True
--- appui/quotetable_state.py
+++ appui/quotetable_state.py
@@ -1,3 +1,6 @@
+from threading import Thread
+from time import monotonic, sleep
+
 from yfinance import YFinance, YQuote
 
 from ._column import Column
@@ -32,18 +35,46 @@ class QuoteTableState:
         ]
 
         # TODO Another quick and dirty hack. This information will be retrieved from the config file as well.
+        # TODO We should store the column key name rather than the column object
         self._sort_column: Column = self._columns[0]
         self._sort_ascending: bool = True
+        self._query_frequency: int = 10
 
+        self._query_thread_running: bool = False
+        self._query_thread: Thread = Thread(target=self._query_quotes)
+        self._last_query_time = monotonic()
+        self._version: int = 0
 
-        # TODO TEMP TEMP TEMP
-        # This is just to get something to show quickly.
-        self._quotes: list[YQuote] = self._yfin.get_quotes(self._quotes_symbols)
+        self._quotes: list[YQuote] = []
 
     @property
     def columns(self) -> list[Column]:
         """The columns of the quote table."""
         return self._columns
+
+    @property
+    def version(self) -> int:
+        """The version of the quote data."""
+        return self._version
+
+    @property
+    def query_thread_running(self) -> bool:
+        """
+        Whether the quote query thread is currently running.
+        """
+        return self._query_thread_running
+
+    @query_thread_running.setter
+    def query_thread_running(self, value: bool) -> None:
+        if value == self._query_thread_running:
+            return
+
+        self._query_thread_running = value
+        if self._query_thread_running:
+            self._last_query_time = monotonic()
+            self._query_thread.start()
+        else:
+            self._query_thread.join()
 
     def get_quotes(self) -> list[list[str]]:
         """
@@ -64,3 +95,19 @@ class QuoteTableState:
         ]
 
         return quote_info
+
+    def _query_quotes(self) -> None:
+        """
+        Query for the quotes and update the change version.
+        """
+        now: float = monotonic()
+        while self._query_thread_running:
+            self._quotes = self._yfin.get_quotes(self._quotes_symbols)
+            self._last_query_time = now
+            self._version += 1
+
+            while now - self._last_query_time < self._query_frequency:
+                if not self._query_thread_running:
+                    return
+                sleep(1)
+                now = monotonic()
