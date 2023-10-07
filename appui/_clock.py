b76859d358a10060d7e9b467034fdd9179089779
Jocelyn Legault <jocelynlegault@gmail.com>
2023-10-07 10:50:40 -0400

    Simplify simple clock widget

--- appui/_clock.py
+++ appui/_clock.py
@@ -3,7 +3,6 @@ A very simple and limited widget to display the current time.
 """
 from datetime import datetime, timezone
 
-from textual.reactive import reactive
 from textual.widgets import Static
 
 
@@ -11,8 +10,6 @@ class Clock(Static):
     """
     A very simple and limited widget to display the current time.
     """
-
-    dirty: reactive = reactive(1)
 
     def on_mount(self) -> None:
         """
@@ -23,12 +20,6 @@ class Clock(Static):
     def update_time(self) -> None:
         """
         Method to tick the clock update.
-        """
-        self.dirty = self.dirty + 1
-
-    def watch_dirty(self, _: bool) -> None:
-        """
-        Called when the time needs to be updated.
         """
         self.update(
             datetime.strftime(datetime.now(timezone.utc).astimezone(), "%H:%M:%S")
