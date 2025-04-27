c1753cc0bb8544802f0857b88f2afc5591dcf907
Jocelyn Legault <jocelynlegault@gmail.com>
2025-04-27 18:20:07 -0400

    Create a start point to start experimenting

---
+++ src/expui/__init__.py
@@ -0,0 +1,8 @@
+"""AppUI provides the TUI for the stockyard application."""
+
+from .expui import ExpUI
+
+__all__ = [
+    "ExpUI",
+]
+__version__ = "0.1.0"
--- src/stockyard.py
+++ src/stockyard.py
@@ -1,12 +1,23 @@
 """Main entry point for the application."""
 
+import argparse
 from pathlib import Path
 
 from appui import StockyardApp
+from expui import ExpUI
+
+parser = argparse.ArgumentParser(description="Stockyard Application")
+parser.add_argument("-e", "--exp", action="store_true", help="Use experimental UI")
+args = parser.parse_args()
 
 home_dir: Path = Path("~").expanduser()
 config_file_name: str = (home_dir / ".stockyard").as_posix()
 
-app: StockyardApp = StockyardApp(config_file_name)
-app.run()
-app.save_config()
+if args.exp:
+    exp_app: ExpUI = ExpUI()
+    exp_app.run()
+else:
+    app: StockyardApp = StockyardApp()
+    app.load_config(config_file_name)
+    app.run()
+    app.save_config(config_file_name)
