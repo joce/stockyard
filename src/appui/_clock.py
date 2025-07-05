601a00bf7732bade7b2c16602c74afe08f4b727e
Jocelyn Legault <jocelynlegault@gmail.com>
2025-07-05 18:48:20 -0400

    Update the clock on OS's boundary

--- src/appui/_clock.py
+++ src/appui/_clock.py
@@ -1,6 +1,7 @@
 """A very simple and limited widget to display the current time."""
 
 from datetime import datetime, timezone
+from time import time
 
 from textual.widgets import Static
 
@@ -12,12 +13,25 @@ class Clock(Static):
         """Event handler called when widget is added to the app."""
 
         # TODO Make the time display configurable through the config file (24h v. AM/PM)
-        # TODO Make the clock update on the system's second change
         self._show_time()
-        self.set_interval(
-            1,
-            self._show_time,
-        )
+        self._schedule_next_update()
+
+    def _schedule_next_update(self) -> None:
+        """Schedule the next update to occur at the next second boundary."""
+
+        # Calculate time until next second boundary
+        now = time()
+        next_second = int(now) + 1
+        delay = next_second - now
+
+        # Schedule the update
+        self.set_timer(delay, self._show_time_and_reschedule)
+
+    def _show_time_and_reschedule(self) -> None:
+        """Update the time and schedule the next update."""
+
+        self._show_time()
+        self._schedule_next_update()
 
     def _show_time(self) -> None:
         """Update the time displayed by the widget."""
